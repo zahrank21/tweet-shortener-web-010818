173e80302796194eb80650ec28d5e9e9196ff132
zahrank21 <zahrank@gmail.com>
2018-01-07 16:21:40 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+def dictionary
+ dict = {
+   "hello" => 'hi',
+   "to" => '2',
+    "two" => '2',
+    "too" => '2',
+   "for" => '4',
+   "four" => '4',
+   'be' => 'b',
+   'you' => 'u',
+   "at" => "@" ,
+   "and" => "&"
+ }
+end
+
+dict = {
+  "hello" => 'hi',
+  "to" => '2',
+   "two" => '2',
+   "too" => '2',
+  "for" => '4',
+  "four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@" ,
+  "and" => "&"
+}
+def word_substituter(str)
+
+  str.string
+
+end
